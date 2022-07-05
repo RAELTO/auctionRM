583c04b311e1169606ebff1ed1262df7adf5abcd
Rael <elias-toro@outlook.com>
2022-07-05 14:51:20 -0500

    folders and files structure ready

---
+++ js/main.js
@@ -0,0 +1,24 @@
+//LOGIC
+
+var app = new Vue({
+    el: '#app',
+    data: {
+        users: [],
+    },
+    methods: {
+        abc(){
+
+        },
+    },
+    created(){
+        /*if (localStorage.getItem('users') !== null) {
+            this.newArrUsers = JSON.parse(localStorage.getItem('users'));
+            this.pos = JSON.parse(localStorage.getItem('pos'));
+            this.nPetsAdopted = JSON.parse(localStorage.getItem('nadopt'));
+        }else{
+            this.listData();
+            this.pos = this.pos;
+            this.nPetsAdopted = this.nPetsAdopted;
+        }*/
+    }
+});
