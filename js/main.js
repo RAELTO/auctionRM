d321bd00f34a5862bf936ede6b5c4d9abe080185
Rael <elias-toro@outlook.com>
2022-07-05 16:28:10 -0500

    log-in was updated, users sign up is working without local storage, working in further upgrades

--- js/main.js
+++ js/main.js
@@ -4,10 +4,46 @@ var app = new Vue({
     el: '#app',
     data: {
         users: [],
+        newUser: {
+            name: '',
+            username: '',
+            password: '',
+            email: '',
+            rmp: 0
+        },
+        userinput: '',
+        passinput: '',
+        confirmpass: '',
     },
     methods: {
-        abc(){
-
+        addUser(){
+            if (
+                this.newUser.name.length > 0 && this.newUser.username.length > 0
+                && this.newUser.password.length > 0 && this.newUser.email.length > 0
+                ) {
+                    if (this.confirmpass === this.newUser.password) {
+                        this.users.push({
+                            ...this.newUser
+                        });
+                        this.clearFields();
+                        alert('Your account has been created successfully');
+                    }else{
+                        alert('Password and confirm password doesnt match');
+                    }
+                
+            }else{
+                alert('Pleas fill all the fields');
+            }
+        },
+        clearFields(){
+            this.newUser = {
+                name: '',
+                username: '',
+                password: '',
+                email: '',
+                rmp: 0
+            }
+            this.confirmpass = '';
         },
     },
     created(){
