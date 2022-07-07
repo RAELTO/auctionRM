69a936d4da337f72900243c421250082cd965779
Rael <elias-toro@outlook.com>
2022-07-07 02:17:48 -0500

    the most part of functionalities are now complete

--- js/main.js
+++ js/main.js
@@ -10,7 +10,8 @@ var app = new Vue({
             username: '',
             password: '',
             email: '',
-            rmp: 0
+            rmp: 0,
+            collection: []
         },
         cards: [],
         rarityList: ['common', 'uncommon', 'rare', 'epic', 'legendary'],
@@ -26,7 +27,12 @@ var app = new Vue({
         ],
         pos: '',
         option: '',
-        trigger: 0,
+        trigger: 0,//to be used in packages modals to show or not the return button
+        amount: 0,
+        onMachine: 0,
+        attemptsM: 0,
+        minutes: 2,
+        winner: '',
     },
     methods: {
         addUser(){
@@ -124,7 +130,10 @@ var app = new Vue({
                 return{
                     ...e,
                     rarity: this.rarityList[random(0, 4)],
-                    price: 0
+                    price: 0,
+                    amount: 0,
+                    startCounter: false,
+                    check: false,
                 }
             });
 
@@ -190,12 +199,148 @@ var app = new Vue({
                 this.mensaje("Please select one payment method", "error");
             }
         },
+        bidUp(item){//ofertar
+            if (item.amount <= item.price) {
+                this.mensaje("The amount must be greater than the actual price", "error");
+            }else{
+                if (this.usession[0].rmp < item.price) {
+                    this.mensaje("Insufficient RMP, please buy more", "error");
+                }else{
+                    const pos = this.users.findIndex((object) => {
+                        return object.username == this.usession[0].username;
+                    });
+                    //this.users[pos].rmp -= item.amount;
+                    this.usession[0].rmp -= item.amount;
+                    item.price = Math.round(item.amount*1.1);
+                    this.mensaje("Your bid has been made!", "success");
+                    item.startCounter = true;
+                    this.winner = this.usession[0].username;
+
+                    if(this.attemptsM == 0){
+                        this.users[pos].rmp = this.usession[0].rmp;
+                        console.log('HOLA AQUI');
+                        this.updateLocalStorage();
+                    }
+
+                    this.onMachine = 1;
+                    if (this.onMachine == 1) {
+                        if (this.attemptsM > 0) {
+                            this.attemptsM -= 1;
+                            function machineBid(){
+                                item.price = Math.round(item.price*1.1);
+                                this.winner = '',
+                                alert('Oops the machine has made a new bid');
+                            }
+                            setTimeout(function(){ machineBid(); }, 2500);
+                        }else{
+                            this.onMachine = 0;
+                            item.check = true;
+                            this.updateLocalStorage();
+                        }
+                    }
+                }
+            }
+        },
+        setWinner(item){
+            const pos = this.users.findIndex((object) => {
+                return object.username == this.usession[0].username;
+            });
+            if (this.winner === this.usession[0].username) {
+                const date = new Date();
+                this.users[pos].collection.push({...item, qty: 1, datePurchased: date.toLocaleDateString()});
+                this.users[pos].collection = this.users[pos].collection.reduce((acc, cv) => {
+                    const elementExists = acc.find(e => e.name === cv.name);
+                    if (elementExists) {
+                    return acc.map((e) => {
+                        if (e.name === cv.name) {
+                        return {
+                            ...e,
+                            qty: e.qty + cv.qty,
+                            amount: (e.amount + cv.amount)/2
+                        }
+                        }
+                        return e;
+                    });
+                    }
+                    return [...acc, cv];
+                }, []);
+                this.usession[0] = this.users[pos];
+                this.updateLocalStorage();
+                this.resetCard(item);
+                this.mensaje("Congrats! the card is now yours!", "success");
+            }else if(this.winner == ''){
+                this.mensaje("Oops The machine has win the bid, better luck the next time", "warning");
+            }
+        },
+        getOut(item){
+            const swalWithBootstrapButtons = Swal.mixin({
+                customClass: {
+                    confirmButton: 'btn btn-success',
+                    cancelButton: 'btn btn-danger'
+                },
+                buttonsStyling: false
+            })       
+            swalWithBootstrapButtons.fire({
+                title: 'Do you want to quit from this auction?',
+                text: "If yes, Your Points will be returned",
+                icon: 'warning',
+                showCancelButton: true,
+                confirmButtonText: 'Yes, Quit',
+                cancelButtonText: 'No, Cancel',
+                reverseButtons: true
+            }).then((result) => {
+                if (result.isConfirmed) {
+                    const pos = this.users.findIndex((object) => {
+                        return object.username == this.usession[0].username;
+                    });
+                    this.usession[0].rmp = this.users[pos].rmp;
+                    this.amount = 0;
+                    this.resetCard(item);
+                    this.mensaje("Your points were returned", "success");
+                    this.updateLocalStorage();
+                } else if (
+                    result.dismiss === Swal.DismissReason.cancel
+                ) {
+                }
+            })
+
+        },
+        resetCard(item){
+            item.check = false;
+            item.startCounter = false;
+            item.amount = 0;
+            switch (item.rarity) {
+                case "common":
+                    item.price = 100;
+                    break;
+                case "uncommon":
+                    item.price = 150;
+                    break;
+                case "rare":
+                    item.price = 200;
+                    break;
+                case "epic":
+                    item.price = 250;
+                    break;
+                case "legendary":
+                    item.price = 300;
+                    break;
+
+                default:
+                    break;
+            }
+            function random(min, max) {
+                return Math.floor((Math.random() * (max - min + 1)) + min);
+            }
+            this.attemptsM = random(1,4);
+            this.updateLocalStorage();
+        },
         mensaje: function (msj, icono) {
             const Toast = Swal.mixin({
                 toast: true,
                 position: 'top-center',
                 showConfirmButton: false,
-                timer: 2000,
+                timer: 1500,
                 timerProgressBar: true,
                 didOpen: (toast) => {
                     toast.addEventListener('mouseenter', Swal.stopTimer)
@@ -211,6 +356,7 @@ var app = new Vue({
         updateLocalStorage(){
             localStorage.setItem('users', JSON.stringify(this.users));
             localStorage.setItem('usession', JSON.stringify(this.usession));
+            localStorage.setItem('cards', JSON.stringify(this.cards));
         },
     },
     created(){
@@ -225,9 +371,17 @@ var app = new Vue({
             this.usession = [];
         }
 
-        this.listCards();
+        if (localStorage.getItem('cards') !== null) {
+            this.cards = JSON.parse(localStorage.getItem('cards'));
+        }else{
+            this.listCards();
+        }
+
+        function random(min, max) {
+            return Math.floor((Math.random() * (max - min + 1)) + min);
+        }
+
+        this.attemptsM = random(1,4);
+        console.log(this.attemptsM);
     },
-    addHistory(item){
-        this.historial
-    }
 });
